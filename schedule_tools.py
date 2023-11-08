3a8f9b905ea96785240c964b087e6ae6d8679c06
qdiaps <turtttle.37@gmail.com>
2023-11-08 20:22:20 +0200

    Написав методи для додавання школи, класу та розкладу

--- schedule_tools.py
+++ schedule_tools.py
@@ -5,6 +5,8 @@ Copyright (c) 2023 qdiaps
 Детальніше дивіться у файлі LICENSE.
 """
 
+default_days = ['1. Понеділок', '2. Вівторок', '3. Середа', '4. Четверг', '5. П\'ятниця', '6. Субота']
+
 def delete_schedule():
 	pass
 
@@ -23,11 +25,19 @@ def edit_class():
 def edit_school():
 	pass
 
-def add_new_schedule():
-	pass
+def add_new_schedule(school_name, class_name, day, schedules, obj):
+   if school_name in obj:
+      if class_name in obj[school_name]:
+         obj[school_name][class_name][day] = schedules
 
-def add_new_class():
-	pass
+def add_new_class(school_name, class_name, obj):
+   if school_name in obj:
+      if class_name not in obj:
+         obj[school_name][class_name] = {}
+         for day in default_days:
+            obj[school_name][class_name][day] = []
 
-def add_new_school():
-	pass
+def add_new_school(school_name, obj):
+   if school_name not in obj:
+      obj[school_name] = {}
+      
