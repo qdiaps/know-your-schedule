25d6c21d432cd3cc93e53a38fbfc505705e9321d
qdiaps <turtttle.37@gmail.com>
2023-11-12 11:48:59 +0200

    Рефакторинг

--- data/json_tools.py
+++ data/json_tools.py
@@ -1,23 +1,24 @@
 import json
 import os
 
-def create_path(path: str):
+def create_path(path: str) -> str:
 	return f'data/{path}'
 
-def serialization(to_json, path: str):
+def serialization(to_json: object, path: str) -> None:
 	json_path = create_path(path)
 	with open(json_path, 'w') as file:
 		json.dump(to_json, file, ensure_ascii=False, sort_keys=True, indent=2)
 
-def deserialization(path: str):
+def deserialization(path: str) -> object:
 	json_path = create_path(path)
+	from_json = None
 	if os.path.isfile(json_path):
 		try:
 			with open(json_path) as file:
-				return json.load(file)
+				from_json = json.load(file)
 		except Exception:
 			print('Exception: json_tools.py -> deserialization()')
 		finally:
-			return 
+			return from_json
 	else:
 		open(json_path, 'w')
