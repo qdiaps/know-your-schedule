bbf2395095f3255df5c025d5bb75418e45056e6e
qdiaps <turtttle.37@gmail.com>
2023-11-12 11:55:14 +0200

    Додав методи видалення школи/класу/розкладу

--- data/schedule_tools.py
+++ data/schedule_tools.py
@@ -1,13 +1,30 @@
-default_days = ['1. Понеділок', '2. Вівторок', '3. Середа', '4. Четверг', '5. П\'ятниця', '6. Субота']
+default_days = [
+   '1. Понеділок', '2. Вівторок', 
+   '3. Середа', '4. Четверг', 
+   '5. П\'ятниця', '6. Субота'
+]
 
-def delete_schedule():
-	pass
+def delete_schedule(
+      school_name: str, 
+      class_name: str, 
+      day: str, 
+      obj: object) -> None:
+   if school_name in obj:
+      if class_name in obj[school_name]:
+         if day in obj[school_name][class_name]:
+            obj[school_name][class_name][day] = []
 
-def delete_class():
-	pass
+def delete_class(
+      school_name: str, 
+      class_name: str, 
+      obj: object) -> None:
+   if school_name in obj:
+      if class_name in obj[school_name]:
+         del obj[school_name][class_name]
 
-def delete_school():
-	pass
+def delete_school(school_name: str, obj: object) -> None:
+   if school_name in obj:
+      del obj[school_name]
 
 def edit_schedule():
 	pass
@@ -18,19 +35,27 @@ def edit_class():
 def edit_school():
 	pass
 
-def add_new_schedule(school_name, class_name, day, schedules, obj):
+def add_new_schedule(
+      school_name: str, 
+      class_name: str, 
+      day: str, 
+      schedules: list, 
+      obj: object) -> None:
    if school_name in obj:
       if class_name in obj[school_name]:
          obj[school_name][class_name][day] = schedules
 
-def add_new_class(school_name, class_name, obj):
+def add_new_class(
+      school_name: str, 
+      class_name: str, 
+      obj: object) -> None:
    if school_name in obj:
       if class_name not in obj:
          obj[school_name][class_name] = {}
          for day in default_days:
             obj[school_name][class_name][day] = []
 
-def add_new_school(school_name, obj):
+def add_new_school(school_name: str, obj: object) -> None:
    if school_name not in obj:
       obj[school_name] = {}
       
