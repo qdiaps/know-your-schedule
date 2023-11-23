ba0611c34b84c9df628db52c8972a024631d6908
qdiaps <turtttle.37@gmail.com>
2023-11-23 18:50:03 +0200

    Фікс проблеми з None та додав в .gitignore ігнорування Json файлів

--- data/json_tools.py
+++ data/json_tools.py
@@ -1,24 +1,24 @@
 import json
 import os
 
-def create_path(path: str) -> str:
-	return f'data/{path}'
+def create_path(file_name: str) -> str:
+	return f'data/{file_name}'
 
-def serialization(to_json: object, path: str) -> None:
-	json_path = create_path(path)
-	with open(json_path, 'w') as file:
-		json.dump(to_json, file, ensure_ascii=False, sort_keys=True, indent=2)
+def serialization(to_json: object, file_name: str) -> None:
+	path = create_path(file_name)
+	with open(path, 'w') as file:
+		json.dump(to_json, file, ensure_ascii=False, indent=2)
 
-def deserialization(path: str) -> object:
-	json_path = create_path(path)
-	from_json = None
-	if os.path.isfile(json_path):
+def deserialization(file_name: str) -> object:
+	path = create_path(file_name)
+	from_json = {}
+	if os.path.isfile(path):
 		try:
-			with open(json_path) as file:
+			with open(path) as file:
 				from_json = json.load(file)
 		except Exception:
 			print('Exception: json_tools.py -> deserialization()')
 		finally:
 			return from_json
 	else:
-		open(json_path, 'w')
+		open(path, 'w')
