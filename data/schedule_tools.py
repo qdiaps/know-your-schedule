40f0179a9d3910550b8e60fd248a425dddd74e47
qdiaps <turtttle.37@gmail.com>
2023-11-23 19:11:45 +0200

    Прибрав зайві пробіли та додав в if методи keys()

--- data/schedule_tools.py
+++ data/schedule_tools.py
@@ -1,29 +1,20 @@
 default_days = [
-   '1. Понеділок', '2. Вівторок', 
-   '3. Середа', '4. Четверг', 
-   '5. П\'ятниця', '6. Субота'
+   '1. Понеділок', '2. Вівторок', '3. Середа', 
+   '4. Четверг', '5. П\'ятниця', '6. Субота'
 ]
 
-def delete_schedule(
-      school_name: str, 
-      class_name: str, 
-      day: str, 
-      obj: object) -> None:
-   if school_name in obj:
-      if class_name in obj[school_name]:
-         if day in obj[school_name][class_name]:
-            obj[school_name][class_name][day] = []
+def delete_schedule(school_name: str, class_name: str, day: str, obj: dict) -> None:
+   if school_name in obj.keys():
+      if class_name in obj[school_name].keys():
+         obj[school_name][class_name][day] = []
 
-def delete_class(
-      school_name: str, 
-      class_name: str, 
-      obj: object) -> None:
-   if school_name in obj:
-      if class_name in obj[school_name]:
+def delete_class(school_name: str, class_name: str, obj: dict) -> None:
+   if school_name in obj.keys():
+      if class_name in obj[school_name].keys():
          del obj[school_name][class_name]
 
-def delete_school(school_name: str, obj: object) -> None:
-   if school_name in obj:
+def delete_school(school_name: str, obj: dict) -> None:
+   if school_name in obj.keys():
       del obj[school_name]
 
 def edit_schedule():
@@ -35,27 +26,19 @@ def edit_class():
 def edit_school():
 	pass
 
-def add_new_schedule(
-      school_name: str, 
-      class_name: str, 
-      day: str, 
-      schedules: list, 
-      obj: object) -> None:
-   if school_name in obj:
-      if class_name in obj[school_name]:
+def add_new_schedule(school_name: str, class_name: str, day: str, schedules: list, obj: dict) -> None:
+   if school_name in obj.keys():
+      if class_name in obj[school_name].keys():
          obj[school_name][class_name][day] = schedules
 
-def add_new_class(
-      school_name: str, 
-      class_name: str, 
-      obj: object) -> None:
-   if school_name in obj:
-      if class_name not in obj:
+def add_new_class(school_name: str, class_name: str, obj: dict) -> None:
+   if school_name in obj.keys():
+      if class_name not in obj[school_name].keys():
          obj[school_name][class_name] = {}
          for day in default_days:
             obj[school_name][class_name][day] = []
 
-def add_new_school(school_name: str, obj: object) -> None:
-   if school_name not in obj:
+def add_new_school(school_name: str, obj: dict) -> None:
+   if school_name not in obj.keys():
       obj[school_name] = {}
       
